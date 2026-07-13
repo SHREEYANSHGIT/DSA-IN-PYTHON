cd0c1bef5962641f6dba41dcb7e701abc60f7ad0
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-07-13 23:29:40 +0530

    Time: 15 ms (32.72%), Space: 20.1 MB (7.97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,23 +1,25 @@
-import heapq
-
 class Solution(object):
     def topKFrequent(self, nums, k):
 
+        # Step 1: Frequency Map
         hashmap = {}
 
         for num in nums:
-            hashmap[num] = hashmap.get(num,0)+1
-        
-        heap = []
-        for num,freq in hashmap.items():
-            heapq.heappush(heap,(freq,num))
-            if len(heap)>k:
-                heapq.heappop(heap)
-            
+            hashmap[num] = hashmap.get(num, 0) + 1
+
+        # Step 2: Create Buckets
+        bucket = [[] for _ in range(len(nums) + 1)]
+
+        # Step 3: Fill Buckets
+        for num, freq in hashmap.items():
+            bucket[freq].append(num)
+
+        # Step 4: Collect Answer
         ans = []
 
-        while heap:
-            e = heapq.heappop(heap)[1]
-            ans.append(e)
+        for freq in range(len(bucket) - 1, 0, -1):
+            for num in bucket[freq]:
+                ans.append(num)
 
-        return ans
+                if len(ans) == k:
+                    return ans
