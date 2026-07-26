ddc01e9109e1c357f5540a49be45d8d8494471f1
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-07-26 13:14:35 +0530

    Time: 5 ms (12.32%), Space: 12.6 MB (81.99%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,39 +1,25 @@
-from collections import deque
-
 class Solution(object):
     def isBipartite(self, graph):
-
         V = len(graph)
-        visited = [0] * V
-
+        visited = [0]*V
         for i in range(V):
+            if visited[i]==0:
+                queue = deque()
+                queue.append(i)
+                visited[i] = "r"
 
-            if visited[i] != 0:
-                continue
-
-            queue = deque()
-            queue.append(i)
-            visited[i] = "r"
-
-            while queue:
-
-                node = queue.popleft()
-
-                for nb in graph[node]:
-
-                    if visited[nb] == "r" and visited[node] == "r":
-                        return False
-
-                    if visited[nb] == "b" and visited[node] == "b":
-                        return False
-
-                    if visited[nb] == 0:
-
-                        if visited[node] == "r":
-                            visited[nb] = "b"
-                        else:
-                            visited[nb] = "r"
-
-                        queue.append(nb)
-
+                while queue:
+                    node = queue.popleft()
+                    for nb in graph[node]:
+                        if visited[nb] == visited[node]:
+                            return False
+                        elif visited[node]=="r" and visited[nb] == 0:
+                            visited[nb]="b"
+                            queue.append(nb)
+                        elif visited[node]=="b" and visited[nb] == 0:
+                            visited[nb]="r"
+                            queue.append(nb)
+                
         return True
+
+
