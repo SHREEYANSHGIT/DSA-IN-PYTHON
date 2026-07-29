58995b88834177287ac4cae3059839a19f20624f
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-07-30 00:36:08 +0530

    Time: 41 ms (91.58%), Space: 17.7 MB (70.65%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,39 +1,35 @@
-from collections import deque
-
 class Solution(object):
+    def dfs(self,curr_node,visited,path_visited,safe,v,graph):
+        visited[curr_node]=1
+        path_visited[curr_node]=1
+        for nb in graph[curr_node]:
+            if visited[nb]==0:
+                ans = self.dfs(nb,visited,path_visited,safe,v,graph)
+                if ans == False:
+                    return False
+            if path_visited[nb] == 1:
+                return False
+        safe[curr_node] = 1
+        path_visited[curr_node]=0
+        return True
+
     def eventualSafeNodes(self, graph):
+        v = len(graph)
+        visited = [0]*v
+        path_visited = [0]*v
+        safe = [0]*v
 
-        n = len(graph)
-
-        reverseGraph = [[] for _ in range(n)]
-        outdegree = [0] * n
-
-        # Build reversed graph and outdegree
-        for node in range(n):
-            outdegree[node] = len(graph[node])
-            for neighbor in graph[node]:
-                reverseGraph[neighbor].append(node)
-
-        queue = deque()
-
-        # Terminal nodes
-        for i in range(n):
-            if outdegree[i] == 0:
-                queue.append(i)
+        for i in range(v):
+            if visited[i] == 0:
+                self.dfs(i,visited,path_visited,safe,v,graph)
 
         result = []
-
-        while queue:
-
-            node = queue.popleft()
-            result.append(node)
-
-            for parent in reverseGraph[node]:
-
-                outdegree[parent] -= 1
-
-                if outdegree[parent] == 0:
-                    queue.append(parent)
-
-        result.sort()
+        for i in range(v):
+            if safe[i]==1:
+                result.append(i)
         return result
+
+        
+        
+
+        
