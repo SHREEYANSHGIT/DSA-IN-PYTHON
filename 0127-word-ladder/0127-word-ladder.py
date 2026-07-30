526b8ccdeb4b4b48884e1a0df4acf0f33258f9e7
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-07-31 00:35:19 +0530

    Time: 309 ms (38.01%), Space: 12.9 MB (83.29%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,25 +1,30 @@
+from collections import deque
 class Solution(object):
+
     def ladderLength(self, beginWord, endWord, wordList):
-        wordset = set(wordList)
+        wordset = set(wordList)  
 
         if endWord not in wordset:
-            return 0
+            return 0 
 
         queue = deque()
-        queue.append((beginWord,1))
+        queue.append((beginWord, 1))
+
         level = float("inf")
         atoz = "abcdefghijklmnopqrstuvwxyz"
+
         while queue:
-            word , count = queue.popleft()
+            word, count = queue.popleft()
+
             if word == endWord:
-                return count
-                
+                level = min(level, count)
+                continue  
+
             for i in range(len(word)):
                 for c in atoz:
-                    nword = word[:i] + c + word[i+1:]
+                    nword = word[:i] + c + word[i + 1 :]
                     if nword in wordset:
-                        queue.append((nword,count+1))
-                        wordset.remove(nword)
-            
-        
-        return 0
+                        queue.append((nword, count + 1))
+                        wordset.remove(nword)  
+
+        return 0 if level == float("inf") else level
