fa13ac440168ab2383a4a68d8d67b75c365eed6f
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-08-08 01:43:18 +0530

    Time: 3 ms (95.72%), Space: 13.4 MB (51.16%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,44 @@
+import heapq
+
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, k):
+
+        adj = [[] for _ in range(n)]
+
+        for u, v, price in flights:
+            adj[u].append((v, price))
+
+        # At most k stops means at most k+1 flights
+        max_flights = k + 1
+
+        # (cost, node, flights_used)
+        heap = [(0, src, 0)]
+
+        # Best number of flights used when reaching each node
+        distance = [float("inf")] * n
+        distance [src] = 0
+
+        while heap:
+
+            kth, node, cost = heapq.heappop(heap)
+    
+
+            if kth >= max_flights:
+                continue
+
+            for nb, price in adj[node]:
+
+                new_cost = cost + price
+                nkth = kth + 1
+
+                if new_cost < distance[nb]:
+
+                    distance[nb] = new_cost
+
+                    heapq.heappush(
+                        heap,
+                        (nkth, nb, new_cost)
+                    )
+        if distance[dst]!= float("inf"):
+            return distance[dst]
+        return -1
