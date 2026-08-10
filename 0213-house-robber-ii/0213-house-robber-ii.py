391869a60337ca7aab009d1638bb8308b5a6c2b4
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-08-11 01:50:31 +0530

    Time: 4 ms (5.91%), Space: 12.3 MB (91.91%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, index, nums, dp):
+        if index == 0:
+            return nums[index]
+        if index == -1:
+            return 0
+        if dp[index] != -1:
+            return dp[index]
+
+
+        pick = nums[index] + self.solve(index - 2, nums , dp)
+        notpick = self.solve(index - 1, nums,dp)
+        ans = max(pick, notpick)
+        dp[index] = ans
+        return ans
+    
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        n = len(nums)
+
+        dp1 = [-1]*(n-1)
+        dp2 = [-1]*(n-1)
+
+        ans1 = self.solve(n - 2, nums[0 : n - 1] , dp1)
+ 
+        ans2 = self.solve(n - 2, nums[1 : n], dp2)
+
+        return max(ans1, ans2)
