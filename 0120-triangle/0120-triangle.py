aa828401edf938fb722b96d023bddf7ad350a9cd
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-08-25 00:52:17 +0530

    Time: 3 ms (81.47%), Space: 12.8 MB (86.46%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,21 +1,35 @@
+# class Solution(object):
+
+#     def find(self, i, j, triangle,dp):
+
+#         if i == len(triangle) - 1:
+#             return triangle[i][j]
+
+#         if dp[i][j] != "x":
+#             return dp[i][j]
+#         left = self.find(i + 1, j, triangle,dp)
+#         right = self.find(i + 1, j + 1, triangle,dp)
+
+#         dp[i][j] = triangle[i][j] + min(left, right)
+
+#         return dp[i][j]
+
+#     def minimumTotal(self, triangle):
+
+#         n = len(triangle)
+#         dp = [["x"] * len(triangle[i]) for i in range(n)]
+#         return self.find(0, 0, triangle,dp)
+
+
+#most optimal 
 class Solution(object):
 
-    def find(self, i, j, triangle,dp):
-
-        if i == len(triangle) - 1:
-            return triangle[i][j]
-
-        if dp[i][j] != "x":
-            return dp[i][j]
-        left = self.find(i + 1, j, triangle,dp)
-        right = self.find(i + 1, j + 1, triangle,dp)
-
-        dp[i][j] = triangle[i][j] + min(left, right)
-
-        return dp[i][j]
-
-    def minimumTotal(self, triangle):
-
+    def minimumTotal(self, triangle): 
         n = len(triangle)
-        dp = [["x"] * len(triangle[i]) for i in range(n)]
-        return self.find(0, 0, triangle,dp)
+        dp = triangle[-1][:]
+
+        for i in range(n-2 , -1 , -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j] , dp[j+1]) 
+
+        return dp[0] 
