5d402911aec943097f116697d197e56c59537a3c
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-09-05 02:42:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (17.25%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -8,13 +8,12 @@ class Solution(object):
             number += digits[i] 
 
         number += 1
-        m = len(str(number))
         for k in range(n-1 , -1 , -1):
             x = number % 10
             digits[k] = x
 
             number = number // 10
 
-        if  m > n :
+        if  number != 0 :
             return [number] + digits
         return digits 
