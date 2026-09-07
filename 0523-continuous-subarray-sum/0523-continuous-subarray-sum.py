58f463cc5fc0cfc8bf774ef219d5b70574a793ae
Shreeyansh Asati <shreeyanshgithub18@gmail.com>
2026-09-08 00:39:56 +0530

    Time: 57 ms (43.99%), Space: 31.3 MB (60.39%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -2,18 +2,17 @@ class Solution(object):
     def checkSubarraySum(self, nums, k):
         hashmap = {0: -1}
         prefix = 0
-
-        for i in range(len(nums)):
-            prefix += nums[i]
-
+        n = len(nums)
+        
+        for i in range(0,n):
+            prefix += nums[i] 
             remainder = prefix % k
-
             if remainder in hashmap:
-                if i - hashmap[remainder] >= 2:
-                    return True
+                if i - hashmap[remainder]  >=2:
+                    return True 
             else:
                 hashmap[remainder] = i
-
+        
         return False
             
         
